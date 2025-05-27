d62bfb59a7702006f2c81593387ae1cf423caccf
RaulEcija <raulecija7@gmail.com>
2025-05-27 18:40:26 +0200

    actualización consulta nº 13

--- SQL/CONSULTA Nº13.sql
+++ SQL/CONSULTA Nº13.sql
@@ -1,8 +1,7 @@
- /*promedio de duración de las películas para cada
+/* promedio de duración de las películas para cada
 clasificación de la tabla film
- */
- select "rating" , ROUND(AVG("length"), 2) as "Promedio_duracion_pelicula" -- Dejo redondeado el promedio a 2 decimales
- from "film"
- group by "rating"
- order by "Promedio_duracion_pelicula" desc; -- Ordeno de más a menos promedio
- 
+*/
+select "rating", ROUND(AVG("length"), 2) as "Promedio_duracion_pelicula" --Redondeo el promedio a dos decimales
+from "film"
+group by "rating"
+order by "Promedio_duracion_pelicula" desc; -- Ordeno por promedio de mayor a menor
