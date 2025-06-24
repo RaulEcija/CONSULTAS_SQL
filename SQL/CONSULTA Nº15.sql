df5b9f641ebe06b727c94c086144162be0e63278
RaulEcija <raulecija7@gmail.com>
2025-06-24 18:29:17 +0200

    corrección consulta nº 15

--- SQL/CONSULTA Nº15.sql
+++ SQL/CONSULTA Nº15.sql
@@ -1,4 +1,6 @@
 -- Dinero total generado por la empresa
-select SUM("customer_id") as "Total_activo_generado" -- Tengo en cuenta que la suma de la columna customer_id de la tabla clientes es el total generado en dinero para la empresa
-from "customer";
+select SUM("amount") as "Dinero_total_generado" -- Tengo en cuenta que el activo de la tabla clientes es lo generado en dinero para la empresa
+from "payment";
+
+
 
