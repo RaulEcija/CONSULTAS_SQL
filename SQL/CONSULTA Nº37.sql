8bbfa269a5b37260dd9e4231d63b1a580c93386e
RaulEcija <raulecija7@gmail.com>
2025-06-24 18:39:51 +0200

    corrección de consulta nº 37

--- SQL/CONSULTA Nº37.sql
+++ SQL/CONSULTA Nº37.sql
@@ -1,8 +1,11 @@
--- Actor más alto y más bajo
+-- Actor con "id" más alto y también más bajo
 
 select "actor_id", "first_name", "last_name" 
 from "actor"
 where "actor_id" = (select MAX("actor_id") from "actor") 
    OR "actor_id" = (select MIN("actor_id") from "actor")
-order by "actor_id" desc ; 
+order by "actor_id" desc ; /* Se ordena la lista de cara a lo que solicita la consulta,
+ mostrar primero el actor con "id" más alto y después el de "id más bajo."
+ */
+
 
